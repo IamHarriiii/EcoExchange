bdd20b5399c6cbafa84a7aaf49d77a942af3d509
IamHarriiii <iamharriiiii@gmail.com>
2025-05-12 22:08:47 +0530

    feat: update functions

--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -1,4 +1,7 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getFirestore, setDoc, doc } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
@@ -16,3 +19,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+export { auth, db };
